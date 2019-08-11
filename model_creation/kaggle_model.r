d2f49f1ccd1628dfd349536842d0964a3c57b9b1
7446Nguyen <38848175+7446Nguyen@users.noreply.github.com>
2019-08-11 12:54:23 -0500

    Update kaggle_model.r

--- model_creation/kaggle_model.r
+++ model_creation/kaggle_model.r
@@ -1,5 +1,5 @@
 #libraries----
-install.packages(c("skimr","tangram","fastDummies",'olsrr','rlang','forcats','roperators'))
+install.packages(c("skimr","tangram","fastDummies",'olsrr','rlang','forcats','roperators','stats'))
 library(tidyverse) #general data wrangling tools
 library(skimr) #summary stats
 library(tangram) #has is.categorical() function, useful for creating tables
@@ -13,9 +13,10 @@ library(corrplot)
 library(mefa4)
 library(MASS)#studentized residuals
 library(caret)
+library(stats)
 
 
-full_training =ames_train
+full_training = ames_train
 
 #Linearity Check----
 
@@ -84,24 +85,24 @@ vif(full.model)
 
 
 #Remove High VIF Variables----
-full_training_noVIF <- full_training[ -c(1,18,42,43,64:66,69,79,82,86,88,91:95)]
+full_training_noVIF <- full_training[ -c(1,18,42,43,45,64:66,69,76,79,82,86,88,91:95)]
 
 full.model2 <- lm(logSalePrice ~., data = full_training_noVIF)
-summary(full.model2)
+summary(full.model2) #R^2 = 0.9257
 vif(full.model2)
 
 #Outlier Check----
 plot(full.model)
-ols_plot_cooksd_bar(initial_model)
+ols_plot_cooksd_bar(full.model2)
 
 #Outlier Removal----
-full_training_no <- full_training_noVIF[-c(74,266,416,547,548,651,892),] 
-
+full_training_no <- full_training_noVIF[-c(74,266,416,547,548,651,892,922,427,314,1029,127,547),] 
+#cooks plot outliers - 427,314,1029,127,547
 full_training_no$BsmtFinType1 <- relevel(full_training_no$BsmtFinType1, ref = "None")
 
 #Model outliers addressed----
 new_model = lm(logSalePrice~., data = full_training_no)#interaction terms will need to be added here
-summary(new_model) #R^2 = 0.9726
+summary(new_model) #R^2 = 0.9388
 vif(new_model)
 
 
@@ -110,96 +111,124 @@ vif(new_model)
 #Step
 stepAIC(new_model, direction = "both")
 
-stepmodel = lm(logSalePrice ~  MSSubClass + MSZoning + LotFrontage + 
-                 LotArea + Utilities + Neighborhood + Condition1 + HouseStyle + 
-                 OverallQual + OverallCond + Exterior1st + MasVnrType + MasVnrArea + 
-                 ExterQual + ExterCond + Foundation + BsmtQual + TotalBsmtSF + 
-                 Heating + HeatingQC + CentralAir + LowQualFinSF + GrLivArea + 
-                 BsmtFullBath + BsmtHalfBath + HalfBath + KitchenQual + TotRmsAbvGrd + 
-                 Functional + Fireplaces + FireplaceQu + GarageType + GarageFinish + 
-                 GarageCars + GarageArea + GarageQual + PavedDrive + ScreenPorch + 
-                 Fence + MoSold + SaleType + SaleCondition + SalePrice + pool_yn + 
-                 OpenPorchSF_group + EnclosedPorch_group + BedroomAbvGr4plus + 
-                 TotRmsAbvGrd10plus-ID, data = full_training_no)
+stepmodel = lm(logSalePrice ~ MSZoning + LotFrontage + LotArea + 
+                 Utilities + LotConfig + LandSlope + Neighborhood + Condition1 + 
+                 Condition2 + BldgType + OverallQual + OverallCond + RoofMatl + 
+                 Exterior1st + Exterior2nd + MasVnrArea + ExterCond + Foundation + 
+                 BsmtQual + BsmtExposure + BsmtFinType1 + BsmtFinSF1 + BsmtFinType2 + 
+                 BsmtFinSF2 + TotalBsmtSF + Heating + HeatingQC + CentralAir + 
+                 BsmtFullBath + FullBath + HalfBath + KitchenQual + TotRmsAbvGrd + 
+                 Functional + Fireplaces + GarageType + GarageCars + GarageArea + 
+                 GarageCond + PavedDrive + ScreenPorch + MiscVal + SaleType + 
+                 SaleCondition + WoodDeckSF_group + pool_yn + porchArea + 
+                 MultipleFloor, data = full_training_no)
 
-summary(stepmodel)#r^2=0.9723
+summary(stepmodel)#r^2=0.9405
 
 #Forwards
 stepAIC(new_model, direction = "forward")
-forwards = lm(logSalePrice ~  MSSubClass + MSZoning + LotFrontage + 
-                LotArea + Street + LotShape + LandContour + Utilities + LotConfig + 
+forwards = lm(logSalePrice ~ MSSubClass + MSZoning + LotFrontage + 
+                LotArea + Street + LandContour + Utilities + LotConfig + 
                 LandSlope + Neighborhood + Condition1 + Condition2 + BldgType + 
                 HouseStyle + OverallQual + OverallCond + YearRemodAdd + RoofStyle + 
                 RoofMatl + Exterior1st + Exterior2nd + MasVnrType + MasVnrArea + 
                 ExterQual + ExterCond + Foundation + BsmtQual + BsmtCond + 
                 BsmtExposure + BsmtFinType1 + BsmtFinSF1 + BsmtFinType2 + 
                 BsmtFinSF2 + BsmtUnfSF + TotalBsmtSF + Heating + HeatingQC + 
-                CentralAir + Electrical + LowQualFinSF + GrLivArea + BsmtFullBath + 
-                BsmtHalfBath + FullBath + HalfBath + KitchenAbvGr + KitchenQual + 
-                TotRmsAbvGrd + Functional + Fireplaces + FireplaceQu + GarageType + 
-                GarageYrBlt + GarageFinish + GarageCars + GarageArea + GarageQual + 
-                GarageCond + PavedDrive + `3SsnPorch` + ScreenPorch + Fence + 
-                MiscVal + MoSold + YrSold + SaleType + SaleCondition + SalePrice + 
-                pool_yn + porchArea + `3SsnPorch_y` + EnclosedPorch_y + ScreenPorch_y + 
-                WoodDeckSF_group + OpenPorchSF_group + EnclosedPorch_group + 
-                BedroomAbvGr4plus + TotRmsAbvGrd10plus + MultipleFloor, data = full_training_no)
-summary(forwards)#r^2=0.9726
+                CentralAir + Electrical + LowQualFinSF + BsmtFullBath + BsmtHalfBath + 
+                FullBath + HalfBath + KitchenAbvGr + KitchenQual + TotRmsAbvGrd + 
+                Functional + Fireplaces + FireplaceQu + GarageType + GarageYrBlt + 
+                GarageFinish + GarageCars + GarageArea + GarageQual + GarageCond + 
+                PavedDrive + X3SsnPorch + ScreenPorch + Fence + MiscVal + 
+                MoSold + YrSold + SaleType + SaleCondition + WoodDeckSF_group + 
+                OpenPorchSF_group + pool_yn + porchArea + X3SsnPorch_y + 
+                EnclosedPorch_y + ScreenPorch_y + BedroomAbvGr4plus + TotRmsAbvGrd10plus + 
+                MultipleFloor, data = full_training_no)
+summary(forwards)#r^2=0.9388
 
 #Backwards
 b.step.model <- stepAIC(new_model, direction = "backward", 
                         trace = FALSE)
-summary(b.step.model)#r^2=0.9726
+summary(b.step.model)#r^2=0.9405
 
 
 #Custom
-custom = lm(logSalePrice ~ Id + MSSubClass + MSZoning + LotFrontage + 
-                 LotArea + Utilities + Neighborhood + Condition1 + HouseStyle + 
-                 OverallQual + OverallCond + Exterior1st + MasVnrType + MasVnrArea + 
-                 ExterQual + ExterCond + Foundation + BsmtQual + TotalBsmtSF + 
-                 Heating + HeatingQC + CentralAir + LowQualFinSF + GrLivArea + 
-                 BsmtFullBath + BsmtHalfBath + HalfBath + KitchenQual + TotRmsAbvGrd + 
-                 Functional + Fireplaces + FireplaceQu + GarageType + GarageFinish + 
-                 GarageCars + GarageArea + GarageQual + PavedDrive + ScreenPorch + 
-                 Fence + MoSold + SaleType + SaleCondition + SalePrice + pool_yn + 
-                 OpenPorchSF_group + EnclosedPorch_group + BedroomAbvGr4plus + 
-                 TotRmsAbvGrd10plus + , data = full_training_no)
+custom = lm(logSalePrice ~ MSZoning + LotFrontage + LotArea + 
+              Utilities + LotConfig + LandSlope + Neighborhood + Condition1 + 
+              Condition2 + BldgType + OverallQual + OverallCond + RoofMatl + 
+              Exterior1st + Exterior2nd + MasVnrArea + ExterCond + Foundation + 
+              BsmtQual + BsmtExposure + BsmtFinType1 + BsmtFinSF1 + BsmtFinType2 + 
+              BsmtFinSF2 + TotalBsmtSF + Heating + HeatingQC + CentralAir + 
+              BsmtFullBath + FullBath + HalfBath + KitchenQual + TotRmsAbvGrd + 
+              Functional + Fireplaces + GarageType + GarageCars + GarageArea + 
+              GarageCond + PavedDrive + ScreenPorch + MiscVal + SaleType + 
+              SaleCondition + WoodDeckSF_group + pool_yn + porchArea + 
+              MultipleFloor + porchArea*Fence , data = full_training_no)
 
-summary(custom)#r^2=0.9724
+summary(custom)#r^2=0.9407
 vif(stepmodel)
 
 #Assumption Check New Model----
-#residual
 
-plot(new_model)
+assumptionPlots<-function(model, data){
+require(c('lindia','ggplot2','gridExtra'))
+  
+# Histogram overlaid with kernel density curve
+hist = ggplot(data, aes(x=predict(model))) + 
+  geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+                 binwidth=.1,
+                 colour="black", fill="white") +
+  geom_density(alpha=.2, fill="#FF6666") +
+  ggtitle("Distribution of Residuals") +
+  theme_bw()
 
-res=resid(new_model)
-plot(training_model_no$"INSERT EXPLAINATORY HERE", res,#insert explanatory here or predicted value???
-     ylab="Residuals", xlab="prices_linlog_nol", 
-     main="prices_linlog_nol Residual Plot")  #change title here
-abline(0, 0, col = 'red')    
+#QQ-plot
+qq = gg_qqplot(model, scale.factor = 1) +
+  ggtitle("Residual QQ-Plot") +
+  theme_bw()
 
+#reisidual Plot
+residp = ggplot(model, aes(.fitted, .resid)) + 
+  geom_point() + 
+  geom_hline(yintercept=0, col="red", linetype="dashed") + 
+  xlab("Fitted values") + 
+  ylab("Residuals") + 
+  ggtitle("Residual vs Fitted Plot") +
+  theme_bw()
 
-#studentized resid
-sresid <- studres(new_model)
-plot(training_model_no$logGrLivArea, sresid,#change explanatory
-     ylab="Studentized Residuals", xlab="prices_linlog_nol", 
-     main="prices_linlog_nol Studentized Residual Plot") 
-abline(2, 0, col = 'red')    
-abline(-2, 0, col = 'red')    
+#standardized residual plots
+sresidp = ggplot(model, aes(.fitted, .stdresid)) + 
+  geom_point() + 
+  geom_hline(yintercept=0, col="red", linetype="dashed") + 
+  xlab("Fitted values") + 
+  ylab("Studentized Residuals") + 
+  ggtitle("StdResid vs Fitted Plot") +
+  theme_bw()+
+  geom_hline(yintercept=c(-2,2),linetype="dashed")
 
+grid.arrange(residp, sresidp, hist, qq, nrow = 1)
+}
 
-#resid hist
-library(MASS)
-resid <- studres(new_model)
-hist(resid, freq=FALSE,
-     main="Distribution of Residuals")
-xfit<-seq(min(resid),max(resid),length=40)
-yfit<-dnorm(xfit)
-lines(xfit, yfit) 
+#Leverage and Outlier Check---- 
+#Cooks
+cooksp = gg_cooksd(full.model2,label = TRUE, show.threshold = TRUE,
+                   threshold = "convention", scale.factor = 0.2)
 
-#qqplot
-qqnorm(res)
-qqline(res, col = "steelblue", lwd = 2)
+#Leverage Plot
+leverage = ggplot(full.model2, aes(.hat, .stdresid))+geom_point(aes(size=.cooksd), na.rm=TRUE,show.legend = FALSE) +
+  stat_smooth(method="loess", na.rm=TRUE) + 
+  xlab("Leverage") + 
+  ylab("Standardized Residuals") + 
+  ggtitle("Residual vs Leverage Plot") + 
+  theme_bw()
+grid.arrange(cooksp, leverage, nrow = 1)
+
+#Model selection assupmtion checks----
+assumptionPlots(full.model2,full_training)
+assumptionPlots(stepmodel,full_training_no)
+assumptionPlots(forwards,full_training_no)
+assumptionPlots(b.step.model,full_training_no)
+assumptionPlots(custom,full_training_no)
 
 #model selection----
 #http://www.sthda.com/english/articles/37-model-selection-essentials-in-r/154-stepwise-regression-essentials-in-r/
